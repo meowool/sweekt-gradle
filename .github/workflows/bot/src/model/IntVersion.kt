bea2b9ab1a14f69dc90a64aebc35059e879d4ded
Meowool Robot <meowool@proton.me>
2023-08-27 00:33:05 +0000

    feat(ci): add workflows for auto syncing upstream
    See [README](https://github.com/meowool/sweekt-gradle/blob/-/.github/workflows/bot/readme.md) for more details.
    
    Committed-by: https://github.com/meowool/sweekt-gradle/actions/runs/5987799648

---
+++ .github/workflows/bot/src/model/IntVersion.kt
@@ -0,0 +1,13 @@
+package com.meowool.sweekt.gradle.model
+
+/**
+ * @author chachako
+ */
+@JvmInline
+value class IntVersion(val number: Int) {
+  constructor(version: String) : this(version.removePrefix("v").toInt())
+
+  companion object {
+    fun String.toIntVersion() = IntVersion(this).number
+  }
+}
